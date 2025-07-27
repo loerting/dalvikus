63d35f7256c083ec57c2f104e0754a105f718da9
loerting <dup2_x2@protonmail.com>
2025-07-27 15:06:06 +0200

    assist popup

--- composeApp/src/commonMain/kotlin/me/lkl/dalvikus/ui/editor/AssistPopup.kt
+++ composeApp/src/commonMain/kotlin/me/lkl/dalvikus/ui/editor/AssistPopup.kt
@@ -2,19 +2,24 @@ package me.lkl.dalvikus.ui.editor
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.focusable
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
@@ -28,9 +33,11 @@ import androidx.compose.ui.input.key.type
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.TextLayoutResult
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Popup
+import androidx.compose.ui.window.PopupProperties
 
 
 data class AssistSuggestion(
@@ -45,6 +52,14 @@ fun AssistPopup(
     textStyle: TextStyle
 ) {
     if (lastLayoutResult == null || !viewModel.isLoaded) return
+
+    var popupDismissed by remember { mutableStateOf(false) }
+
+    LaunchedEffect(viewModel.internalContent) {
+        popupDismissed = false
+    }
+
+    if(popupDismissed) return
 
     val density = LocalDensity.current
     val cursorRect = lastLayoutResult.getCursorRect(viewModel.internalContent.selection.start)
@@ -77,11 +92,20 @@ fun AssistPopup(
 
     Popup(
         offset = with(density) {
+            val iconWidth = 22.dp.roundToPx()
             IntOffset(
-                cursorRect.left.toInt() + 1,
+                cursorRect.left.toInt() + 1 - iconWidth,
                 cursorRect.bottom.toInt() + 1
             )
-        }
+        },
+        properties = PopupProperties(
+            focusable = true,
+            dismissOnBackPress = true,
+            dismissOnClickOutside = true
+        ),
+        onDismissRequest = {
+            popupDismissed = true
+        },
     ) {
         Surface(
             modifier = Modifier
@@ -90,15 +114,13 @@ fun AssistPopup(
             shadowElevation = 2.dp,
             shape = RoundedCornerShape(4.dp)
         ) {
-            androidx.compose.foundation.lazy.LazyColumn(
+            LazyColumn(
                 modifier = Modifier
                     .background(MaterialTheme.colorScheme.surface)
             ) {
                 itemsIndexed(suggestions) { index, suggestion ->
                     val isSelected = index == selectedIndex.value
-                    Text(
-                        text = suggestion.text + (suggestion.description?.let { " - $it" } ?: ""),
-                        style = textStyle,
+                    Row(
                         modifier = Modifier
                             .fillMaxWidth()
                             .background(
@@ -106,7 +128,19 @@ fun AssistPopup(
                                 else MaterialTheme.colorScheme.surface
                             )
                             .padding(3.dp)
-                    )
+                    ) {
+                        Icon(
+                            suggestion.icon, contentDescription = null,
+                            modifier = Modifier.size(16.dp)
+                        )
+                        Text(
+                            text = suggestion.text + (suggestion.description?.let { " - $it" } ?: ""),
+                            style = textStyle,
+                            maxLines = 1,
+                            overflow = TextOverflow.Ellipsis,
+                            modifier = Modifier
+                        )
+                    }
                 }
             }
         }
