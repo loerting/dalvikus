bd422d35cc39d37dc9609d37d520750a5df69c3a
loerting <dup2_x2@protonmail.com>
2025-08-22 16:13:39 +0200

    add chinese opcode translations.

--- composeApp/src/commonMain/kotlin/me/lkl/dalvikus/ui/tree/TreeView.kt
+++ composeApp/src/commonMain/kotlin/me/lkl/dalvikus/ui/tree/TreeView.kt
@@ -8,11 +8,7 @@ import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.ChevronRight
 import androidx.compose.material.icons.outlined.ExpandMore
-import androidx.compose.material3.Icon
-import androidx.compose.material3.ListItem
-import androidx.compose.material3.ListItemDefaults
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -28,14 +24,13 @@ import kotlinx.coroutines.launch
 import me.lkl.dalvikus.LocalSnackbarManager
 import me.lkl.dalvikus.errorreport.crtExHandler
 import me.lkl.dalvikus.tree.ContainerNode
-import me.lkl.dalvikus.tree.Node
 import me.lkl.dalvikus.tree.Metadata
+import me.lkl.dalvikus.tree.Node
+import me.lkl.dalvikus.tree.getPathHistory
 import me.lkl.dalvikus.tree.root.HiddenRoot
 import me.lkl.dalvikus.util.formatFileDate
 import me.lkl.dalvikus.util.formatFileSize
-import org.jetbrains.compose.resources.StringResource
 import org.jetbrains.compose.resources.pluralStringResource
-import org.jetbrains.compose.resources.stringResource
 
 @Composable
 fun TreeView(
@@ -122,6 +117,8 @@ fun TreeView(
                     },
                     onClick = { onFileSelected?.invoke(node) },
                     selected = (node == selectedElement)
+                            // if the selectedElement is from old tree and it was rebuilt, don't lose selection.
+                            || (node.getPathHistory() == selectedElement?.getPathHistory())
                 )
             }
         }
@@ -133,73 +130,6 @@ fun TreeView(
         )
     }
 }
-/*
-@Composable
-private fun TreeRow(
-    node: Node,
-    indent: Int,
-    isExpanded: Boolean,
-    onToggleExpand: suspend (Boolean) -> Unit,
-    onClick: () -> Unit,
-    selected: Boolean
-) {
-    val coroutineScope = rememberCoroutineScope()
-    val isContainer = node is ContainerNode
-
-    Row(
-        modifier = Modifier
-            .requiredHeight(48.dp)
-            .fillMaxWidth()
-            .background(
-                if (selected)
-                    MaterialTheme.colorScheme.primary.copy(alpha = 0.25f)
-                else
-                    Color.Transparent
-            )
-            .clickable {
-                onClick()
-                if (isContainer) {
-                    coroutineScope.launch(Dispatchers.IO + crtExHandler) {
-                        onToggleExpand(!isExpanded)
-                    }
-                }
-            }
-            .padding(start = (4 + indent * 16).dp),
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        when {
-            isContainer -> Icon(
-                imageVector = if (isExpanded) Icons.Outlined.ExpandMore else Icons.Outlined.ChevronRight,
-                contentDescription = null,
-                tint = MaterialTheme.colorScheme.onSurface
-            )
-
-            else -> Spacer(modifier = Modifier.size(24.dp))
-        }
-
-        Spacer(modifier = Modifier.width(4.dp))
-
-        Icon(
-            imageVector = node.icon,
-            contentDescription = node.name,
-            tint = node.color ?: MaterialTheme.colorScheme.onSurface,
-        )
-
-        Spacer(modifier = Modifier.width(8.dp))
-
-        Text(
-            text = node.name,
-            maxLines = 1,
-            overflow = TextOverflow.Ellipsis,
-            style = MaterialTheme.typography.bodyLarge,
-            color = node.color ?: MaterialTheme.colorScheme.onSurface,
-            modifier = Modifier.weight(1f)
-        )
-    }
-}
-
-
-*/
 
 @Composable
 private fun TreeRow(
@@ -269,8 +199,17 @@ private fun TreeRow(
                 when (label) {
                     Metadata.FILE_SIZE -> formatFileSize(value as Long)
                     Metadata.LAST_EDITED -> formatFileDate(value as Long)
-                    Metadata.METHOD_COUNT -> pluralStringResource(Res.plurals.tree_method_count, value as Int, value.toString())
-                    Metadata.FIELD_COUNT -> pluralStringResource(Res.plurals.tree_field_count, value as Int, value.toString())
+                    Metadata.METHOD_COUNT -> pluralStringResource(
+                        Res.plurals.tree_method_count,
+                        value as Int,
+                        value.toString()
+                    )
+
+                    Metadata.FIELD_COUNT -> pluralStringResource(
+                        Res.plurals.tree_field_count,
+                        value as Int,
+                        value.toString()
+                    )
                 }
             }
 
